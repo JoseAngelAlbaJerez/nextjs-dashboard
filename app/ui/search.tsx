7f977dc5d2d2682ab378e9a185727d0241e5281e
JoseAngelAlbaJerez <joseangelalba24@gmail.com>
2024-03-30 19:33:43 -0400

    feat:  created replace and pathname cosnt

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -4,6 +4,8 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
     if (term) {
