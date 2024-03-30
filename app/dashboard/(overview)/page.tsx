030734913ef8e394ebe5c378193cd73f1276f398
JoseAngelAlbaJerez <joseangelalba24@gmail.com>
2024-03-30 19:11:40 -0400

    fix: fetchcardData function removed and CardSkeleton imported

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,16 +4,12 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import {  fetchLatestInvoices,fetchCardData, } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton,CardsSkeleton, } from '@/app/ui/skeletons';
 export default async function Page() {
 
   const latestInvoices = await fetchLatestInvoices();
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
+
+
   return (
     <main>
     <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
