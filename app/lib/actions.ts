2b87ba8f275bfdc92c46ba3c81e290f148a1b299
JoseAngelAlbaJerez <joseangelalba24@gmail.com>
2024-03-30 20:05:50 -0400

    feat: adding redirection

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,7 +2,7 @@
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
-
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -27,4 +27,5 @@ export async function createInvoice(formData: FormData) {
     `;
 
     revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
